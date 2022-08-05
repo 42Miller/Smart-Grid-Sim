9c14bcb227b3ade6437368c4d8012dd7475803b0
42miller <69922322+42miller@users.noreply.github.com>
2022-08-05 17:21:32 -0400

    1 year consumption test

--- src/Building.py
+++ src/Building.py
@@ -1,12 +1,54 @@
+import random
+import time
+
 class Building:
-    def __init__(self, area_sqr_ft, vol_cubic_ft):
-        self.area = area_sqr_ft
-        self.volume = vol_cubic_ft
-        self.power_req = 100
+    def __init__(self):
+        pass
         
+    #returns power required in (kW-hr)
     def getPowerReq(self):
         return self.power_req
         
         
 class House(Building):
-    pass
+    def __init__(self):
+        # Average household usage based on data from US Energy Information Administration:
+        # https://www.eia.gov/tools/faqs/faq.php?id=96&t=3
+        # Value calc'd by us avg consumption / 123.53 million housing units
+        # Electricity Only (kW-hr)
+        self.electric_only = {}
+        self.electric_only["Space_Cooling"] = 1902.37
+        self.electric_only["Refrigeration"] = 704.28
+        self.electric_only["Cooking"] = 129.52
+        self.electric_only["Clothes_Dryers"] = 518.09
+        self.electric_only["Freezers"] = 161.90
+        self.electric_only["Lighting"] = 477.62
+        self.electric_only["Clothes_Washers"] = 89.05
+        self.electric_only["Dishwashers"] = 64.76
+        self.electric_only["Televisions_and_Related_Equipment"] = 453.33
+        self.electric_only["Computers_and_Related_Equipment"] = 291.43
+        self.electric_only["Furnace_Fans_and_Boiler_Circulation_Pumps"] = 194.28
+        self.electric_only["Other_Uses"] = 4209.50
+        
+        # Multiple Sources (kW-hr)
+        # Percentage is based on electrical energy / total energy for each category
+        # https://www.eia.gov/consumption/residential/data/2020/index.php?view=characteristics
+        self.multiple_source = {}
+        self.multiple_source["Space_Heating"] = 1675.71
+        self.multiple_source["percent_Space_Heating"] = 0.4163 #41.63 percent of homes use electricity for space heating
+        self.multiple_source["Water_Heating"] = 1424.76
+        self.multiple_source["percent_Water_Heating"] = 0.4715
+        
+        random.seed(time.time())
+        self.multiple_source["electric_Space_Heating"] = (random.random() <= self.multiple_source["percent_Space_Heating"])
+        self.multiple_source["electric_Water_Heating"] = (random.random() <= self.multiple_source["percent_Water_Heating"])
+        
+        self.power_req = 0
+        for key in self.electric_only.keys():
+            self.power_req = self.power_req + self.electric_only[key]
+            
+        for key in self.multiple_source.keys():
+            if not key.startswith("percent") and not key.startswith("electric"):
+                self.power_req = self.power_req + self.multiple_source[key] * self.multiple_source["electric_" + key]
+        
+        Building.__init__(self)
--- src/Sim.py
+++ src/Sim.py
@@ -1,13 +1,31 @@
 from Grid import *
-from Building import House
+import Building
+#import matplotlib.pyplot as plt
+import json
 
-house1 = House(1,1)
-house2 = House(1,1)
-house3 = House(1,1)
+house1 = Building.House()
 
 grid = Grid()
 grid.add(house1)
-grid.add(house2)
-grid.add(house3)
-
 print(grid.getPowerReq())
+
+
+
+# # x axis values
+# x = [1,2,3]
+# # corresponding y axis values
+# y = [2,4,1]
+#   
+# # plotting the points 
+# plt.plot(x, y)
+#   
+# # naming the x axis
+# plt.xlabel('x - axis')
+# # naming the y axis
+# plt.ylabel('y - axis')
+#   
+# # giving a title to my graph
+# plt.title('My first graph!')
+#   
+# # function to show the plot
+# plt.show()
